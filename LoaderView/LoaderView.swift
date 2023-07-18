fd180d1a6159ef230b26dcebbee37bcae7deaf61
ALX <alexmonac13@gmail.com>
2023-07-18 22:08:56 +0200

    UI loader animation

---
+++ LoaderView/LoaderView.swift
@@ -0,0 +1,20 @@
+//
+//  LoaderView.swift
+//  LoaderView
+//
+//  Created by Alex on 18/07/2023.
+//
+
+import SwiftUI
+
+struct LoaderView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct LoaderView_Previews: PreviewProvider {
+    static var previews: some View {
+        LoaderView()
+    }
+}
