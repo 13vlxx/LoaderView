0aaa8e931fe87b8356d402705a17b4596da19d42
ALX <alexmonac13@gmail.com>
2023-07-18 21:55:38 +0200

    Initial Commit

---
+++ LoaderView/LoaderViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  LoaderViewApp.swift
+//  LoaderView
+//
+//  Created by Alex on 18/07/2023.
+//
+
+import SwiftUI
+
+@main
+struct LoaderViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
